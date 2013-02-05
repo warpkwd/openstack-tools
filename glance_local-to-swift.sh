9cd41f9537404905b69b9ebf6e3b5b0e718f9f32
emilienm <emilien.macchi@enovance.com>
2013-02-05 15:43:05 +0100

    Improve the script

--- glance_local-to-swift.sh
+++ glance_local-to-swift.sh
@@ -6,26 +6,20 @@
 # Note :    Work only with at least Glance API v2  #
 ####################################################
 
-# We need to be root
-if [[ $EUID != 0 ]]; then
-    echo "This script must be run as root."
-    exit 1;
-fi
-
 #### Configuration ####
 
 # Keystone Admin user authentification
 OS_TENANT_NAME=admin
 OS_USERNAME=admin
-OS_PASSWORD=password
+OS_PASSWORD=secrete
 OS_AUTH_URL="http://localhost:5000/v2.0/"
 
-# Glance Access
-GLANCE_HOST=localhost
+# Glance API Access
+GLANCE_API_HOST=localhost
+GLANCE_USER=root
 
 # Swift Access from Glance user
-SWIFT_ENDPOINT="http://localhost:8080/v1"
-SWIFT_USER=glance
+SWIFT_USER="glance:glance"
 SWIFT_PASSORD=secrete
 
 # Misc
@@ -42,6 +36,7 @@ fi
 
 # Image Download + Image listing with ownership
 
+echo "All images are going to be downloaded :"
 IMAGES_LIST=$(glance image-list > images.list)
 sed -i '1,3d' images.list
 sed -i '$d' images.list
@@ -54,21 +49,36 @@ do
       	STATUS=$(echo $image | awk '{print $12}')
       	OWNER=$(glance image-show $ID | grep owner | awk '{print $4}')
 	echo "$image $OWNER">>images-with-owner.list 
+	echo "Downloading : $NAME.$DISK_FORMAT ..."
       	glance image-download $ID >$NAME.$DISK_FORMAT
 done < images.list
-
 mv images-with-owner.list images.list
 
 # Modify Glance configuration from local to swift storage backend
-sed 's/' /etc/glance/glance-api.conf
-sed 's/' /etc/glance/glance-registry.conf
-
+echo "-> Updating of glance-api.conf"
+ssh $GLANCE_USER@$GLANCE_API_HOST "sed -e 's/\(default_store *= *\).*/swift/' \
+	-e 's/\(swift_store_auth_address *= *\).*/\1$OS_AUTH_URL/' \
+	-e 's/\(swift_store_user *= *\).*/\1$SWIFT_USER/' \
+	-e 's/\(swift_store_key *= *\).*/\1$SWIFT_KEY/' \ 
+	-e 's/\(swift_store_create_container_on_put *= *\).*/True/' /etc/glance/glance-api.conf"
 
 # Restart Glance
-service glance-api restart
-service glance-registry restart
+echo "-> Restarting Glance API Service"
+ssh $GLANCE_USER@$GLANCE_API_HOST "service glance-api restart"
 
+# Upload images into Swift
+while read image
+do
+        ID=$(echo $image | awk '{print $2}')
+        NAME=$(echo $image | awk '{print $4}')
+        DISK_FORMAT=$(echo $image | awk '{print $6}')
+        CONTAINER_FORMAT=$(echo $image | awk '{print $8}')
+        STATUS=$(echo $image | awk '{print $12}')
+	OWNER=$(glance image-show $ID | grep owner | awk '{print $14}')
+	glance image-create --name=$NAME --disk_format=$DISK_FORMAT --container_format=$CONTAINER_FORMAT \
+		--owner=$OWNER --is-public=True < $NAME.$DISK_FORMAT
+done < images.list
 
 # End of the script
 cd .. && rm -rf $TMP_DIR
-echo "Glances images have been moved from local to Swift backend."
+echo "Glance images have been moved from local to Swift backend."
