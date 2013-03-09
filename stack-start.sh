e10f2196a62e8f8a2922f37fbfb7db79f47b473a
Emilien Macchi <emilien.macchi@enovance.com>
2013-03-09 15:00:45 +0100

    Update stack-start.sh
    Delete variables

--- stack-start.sh
+++ stack-start.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 # Need to be add into bashrc of devstack server
 
-login = adminsys
-timeout = 5 # seconds
-
-read -p "Start DevStack ? (y/N)? " -t $timeout stack
+read -p "Start DevStack ? (y/N)? " -t 10 stack
 
 if [[ $stack = "y" ]]; then
-    cd /home/$login/devstack && ./unstack.sh && ./stack.sh;
+    cd /home/adminsys/devstack && ./unstack.sh && ./stack.sh;
     else exit 0;
 fi
