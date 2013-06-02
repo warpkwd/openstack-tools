0832e475d00f2d85fe5838f0ed35ae80b7373012
Émilien Macchi <emilien.macchi@enovance.com>
2013-06-02 23:07:54 +0200

    Add licensing

--- nova-resize-bench.sh
+++ nova-resize-bench.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
+#
+# Benchmark "nova resize"
+#
+# Copyright © 2013 eNovance <licensing@enovance.com>
+#
+# Author: Emilien Macchi <emilien.macchi@enovance.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
 
 # OpenStack Credentials
 export OS_TENANT_NAME=admin
